6aa7d2e2a01d770ac2b55784de661a0713cdb955
root <root@WIN-8PTQVG0N92K.localdomain>
2021-04-25 22:14:45 +0300

    step 9.9.2.0

--- src/Engine.php
+++ src/Engine.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Brain\Games\Engine;
+
 use function cli\line;
 use function cli\prompt;
 
@@ -8,82 +10,61 @@ function greetingPart(string $gameName)
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    if($gameName === "calc")
+    if ($gameName === "calc") {
         line("What is the result of the expression?");
-    elseif($gameName === "even")
+    } elseif ($gameName === "even") {
         line("Answer \"yes\" if the number is even, otherwise answer \"no\".");
-    elseif($gameName === "gcd")
+    } elseif ($gameName === "gcd") {
         line("Find the greatest common divisor of given numbers.");
-    elseif($gameName === "progression")
+    } elseif ($gameName === "progression") {
         line("What number is missing in the progression?");
-    elseif($gameName === "prime")
+    } elseif ($gameName === "prime") {
         line("Answer \"yes\" if given number is prime. Otherwise answer \"no\".");
+    }
 
     return $name;
 }
 
 function gameEvent($cnt, $name, $gameName)
 {
-    if(congratzCheck($cnt, $name))
+    if (congratzCheck($cnt, $name)) {
         return true;
+    }
 
-    if($gameName === "even")
-    {
-        if(gameEven($name, $gameName))
-        {
+
+    if ($gameName === "even") {
+        if (gameEven($name, $gameName)) {
             $cnt = incrementCnt($cnt);
             return gameEvent($cnt, $name, $gameName);
-        }
-        else
-        {
+        } else {
             return false;
         }
-    }
-    elseif($gameName === "calc")
-    {
-        if(gameCalc($name, $gameName))
-        {
+    } elseif ($gameName === "calc") {
+        if (gameCalc($name, $gameName)) {
             $cnt = incrementCnt($cnt);
             return gameEvent($cnt, $name, $gameName);
-        }
-        else
-        {
+        } else {
             return false;
         }
-    }
-    elseif($gameName === "gcd")
-    {
-        if(gameGcd($name, $gameName))
-        {
+    } elseif ($gameName === "gcd") {
+        if (gameGcd($name, $gameName)) {
             $cnt = incrementCnt($cnt);
             return gameEvent($cnt, $name, $gameName);
-        }
-        else
-        {
+        } else {
             return false;
         }
-    }
-    elseif($gameName === "progression")
-    {
-        if(gameProgression($name, $gameName))
-        {
+    } elseif ($gameName === "progression") {
+        if (gameProgression($name, $gameName)) {
             $cnt = incrementCnt($cnt);
             return gameEvent($cnt, $name, $gameName);
-        }
-        else
-        {
+        } else {
             return false;
         }
-    }
-    elseif($gameName === "prime")
-    {
-        if(gamePrime($name, $gameName))
-        {
+    } elseif ($gameName === "prime") {
+        if (gamePrime($name, $gameName)) {
             $cnt = incrementCnt($cnt);
             return gameEvent($cnt, $name, $gameName);
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
@@ -91,56 +72,43 @@ function gameEvent($cnt, $name, $gameName)
 
 function isRightAnswer($question, string $gameName, $questionKey = null)
 {
-    if($gameName == "even")
-    {
-        return $question%2 ==  0 ? "yes" : "no";
-    }
-    elseif($gameName == "calc")
-    {
+    if ($gameName == "even") {
+        return $question % 2 ==  0 ? "yes" : "no";
+    } elseif ($gameName == "calc") {
         [$first, $second, $operator] = $question;
         $answer = null;
-        if($operator == "-")
+        if ($operator == "-") {
             $answer = (int)$first - (int)$second;
-        elseif($operator == "+")
+        } elseif ($operator == "+") {
             $answer = (int)$first + (int)$second;
-        else
+        } else {
             $answer = (int)$first * (int)$second;
+        }
 
         return $answer;
-    }
-    elseif($gameName == "gcd")
-    {
+    } elseif ($gameName == "gcd") {
         [$first, $second] = $question;
-
-        while(true)
-        {
-            if($first == $second)
-            {
+        while (true) {
+            if ($first == $second) {
                 return $second;
             }
-            if($first > $second)
-            {
+            if ($first > $second) {
                 $first -= $second;
-            }
-            else
-            {
+            } else {
                 $second -= $first;
             }
         }
-    }
-    elseif($gameName == "progression")
-    {
-        if(array_key_exists($questionKey, $question))
+    } elseif ($gameName == "progression") {
+        if (array_key_exists($questionKey, $question)) {
             return $question[$questionKey];
-        else
+        } else {
             return false;
-    }
-    elseif($gameName == "prime")
-    {
-        for($i = $question - 1; $i > 1; $i -= 1)
-        {
-            if($question % $i == 0)
+        }
+    } elseif ($gameName == "prime") {
+        for ($i = $question - 1; $i > 1; $i -= 1) {
+            if ($question % $i == 0) {
                 return "no";
+            }
         }
         return "yes";
     }
@@ -148,10 +116,8 @@ function isRightAnswer($question, string $gameName, $questionKey = null)
 
 function congratzCheck($cnt, $name)
 {
-    if($cnt >= 3)
-    {
+    if ($cnt >= 3) {
         line("Congratulations, {$name}!");
-
         return true;
     }
 }
@@ -160,7 +126,6 @@ function congratzCheck($cnt, $name)
 function incrementCnt(int $cnt)
 {
     $cnt = $cnt += 1;
-
     return $cnt;
 }
 
@@ -173,14 +138,10 @@ function gameEven($name, $gameName)
     line("Question: {$question}");
     $answer = prompt("Your answer: ");
 
-    if(isRightAnswer($question, $gameName) == $answer)
-    {
+    if (isRightAnswer($question, $gameName) == $answer) {
         line("Correct!");
-        
         return true;
-    }
-    else
-    {
+    } else {
         $rightAnswer = isRightAnswer($question, $gameName);
         line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
         line("Let's try again, {$name}!");
@@ -196,14 +157,10 @@ function gameCalc($name, $gameName)
     line("Question: {$first} {$operator} {$second}");
     $answer = prompt("Your answer: ");
 
-    if(isRightAnswer($question, $gameName) == $answer)
-    {
+    if (isRightAnswer($question, $gameName) == $answer) {
         line("Correct!");
-        
         return true;
-    }
-    else
-    {
+    } else {
         $rightAnswer = isRightAnswer($question, $gameName);
         line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
         line("Let's try again, {$name}!");
@@ -219,14 +176,10 @@ function gameGcd($name, $gameName)
     line("Question: {$first} {$second}");
     $answer = prompt("Your answer: ");
 
-    if(isRightAnswer($question, $gameName) == $answer)
-    {
+    if (isRightAnswer($question, $gameName) == $answer) {
         line("Correct!");
-        
         return true;
-    }
-    else
-    {
+    } else {
         $rightAnswer = isRightAnswer($question, $gameName);
         line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
         line("Let's try again, {$name}!");
@@ -244,13 +197,10 @@ function gameProgression($name, $gameName)
     $questionSeparated = implode(" ", $questionWithSecret);
     line("Question: {$questionSeparated}");
     $answer = prompt("Your answer: ");
-    if(isRightAnswer($question, $gameName, $questionKey) == $answer)
-    {
+    if (isRightAnswer($question, $gameName, $questionKey) == $answer) {
         line("Correct!");
         return true;
-    }
-    else
-    {
+    } else {
         $rightAnswer = isRightAnswer($question, $gameName, $questionKey);
         line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
         line("Let's try again, {$name}!");
@@ -264,13 +214,10 @@ function gamePrime($name, $gameName)
     $question = getQuestion($gameName);
     line("Question: {$question}");
     $answer = prompt("Your answer: ");
-    if(isRightAnswer($question, $gameName) == $answer)
-    {
+    if (isRightAnswer($question, $gameName) == $answer) {
         line("Correct!");
         return true;
-    }
-    else
-    {
+    } else {
         $rightAnswer = isRightAnswer($question, $gameName);
         line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
         line("Let's try again, {$name}!");
@@ -281,53 +228,43 @@ function gamePrime($name, $gameName)
 
 function getQuestion($gameName)
 {
-    if($gameName === "calc")
-    {
+    if ($gameName === "calc") {
         $result     =   [];
         $result[]   =   random_int(1, 100);
         $result[]   =   random_int(1, 100);
-        if($rand = random_int(1, 3))
-        {
-            if($rand == 1)
+        if ($rand = random_int(1, 3)) {
+            if ($rand == 1) {
                 $result[] = "-";
-            elseif($rand == 2)
+            } elseif ($rand == 2) {
                 $result[] = "+";
-            else
+            } else {
                 $result[] = "*";
+            }
         }
 
         return $result;
-    }
-    elseif($gameName === "even")
-    {
+    } elseif ($gameName === "even") {
         return random_int(1, 100);
-    }
-    elseif($gameName === "gcd")
-    {
+    } elseif ($gameName === "gcd") {
         $result     =   [];
         $result[]   =   random_int(1, 100);
         $result[]   =   random_int(1, 100);
 
         return $result;
-    }
-    elseif($gameName === "progression")
-    {
+    } elseif ($gameName === "progression") {
         $count          =   0;
         $result         =   [];
         $rand           =   random_int(1, 10);
         $progression    =   random_int(1, 10);
-        for($i = 0; $i < 10; $i += 1)
-        {
-            if($i === 0)
+        for ($i = 0; $i < 10; $i += 1) {
+            if ($i === 0) {
                 $result[]   =   $progression + $rand;
-            else
-                $result[]   =   $result[$i-1] + $rand;
+            } else {
+                $result[]   =   $result[$i - 1] + $rand;
+            }
         }
-        
         return $result;
-    }
-    elseif($gameName === "prime")
-    {
+    } elseif ($gameName === "prime") {
         return random_int(1, 100);
     }
 }
